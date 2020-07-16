92f118fd368e5a5e53b23806a88ff7328df1ffb7
vinodborole <vborole@gmail.com>
2020-07-16 16:48:41 +0530

    optimization

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # pg2mysql
 Migrate data from postgres to mysql
 
-## Dump from postgres
+## Dump from postgres (only data)
 
 pg_dump -h localhost -p 5432 -U username -W  --data-only --column-inserts dbName > db.sql
 
@@ -11,17 +11,18 @@ The dump is generated in the form of insert queries
 
 pg2mysql takes following parameters
 
-1. New Database name
-2. New user
-3. New user password
-4. Schema file containing table creation queries
-5. data file -> exported from pg_dump command in the form of all insert queries
+1. New Mysql Database to be created
+2. New Mysql user to be created with privileges for new database
+3. New Mysql user password for the new Mysql user
+4. Port
+5. Mysql compatible Schema file (Create Table queries)
+6. Data file -> exported from pg_dump command that contains all insert queries
 
 ## Example:
 
-./pg2mysql.sh newDBname newDBuser newDBpass schemafile db.sql
+./pg2mysql.sh newMysqlDBname newMysqlDBuser newMysqlDBpass port schemafile db.sql
 
-$ ./pg2mysql.sh testDB foo bar testDB.sql db.sql
+$ ./pg2mysql.sh testDB foo bar 3306 testDB.sql db.sql
 Enter password:
 Database testDB and user foo created with a password bar with schema imported from testDB.sql and data migrated from db.sql
 
--- pg2mysql.sh
+++ pg2mysql.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-# Functions
-ok() { echo -e '\e[32m'$1'\e[m'; } # Green
-
-EXPECTED_ARGS=5
+EXPECTED_ARGS=6
 E_BADARGS=65
 MYSQL=`which mysql`
 
-sed -i '' 's/SET/-- SET/g' $5
-sed -i '' 's/SELECT/-- SELECT/g' $5
-sed -i '' 's/public.//g' $5
-sed -i '' '/schema_migrations/d' $5
-sed -i '' 's/+[[:digit:]]\{2\}:[[:digit:]]\{2\}//g' $5
+NEWDBASE=$1
+NEWUSER=$2
+NEWPASS=$3
 
-Q1="CREATE DATABASE IF NOT EXISTS $1;"
-Q2="GRANT ALL ON *.* TO '$2'@'localhost' IDENTIFIED BY '$3';"
+PORT=$4
+HOST='localhost'
+
+sed -i '' 's/SET/-- SET/g' $6
+sed -i '' 's/SELECT/-- SELECT/g' $6
+sed -i '' 's/public.//g' $6
+sed -i '' '/schema_migrations/d' $6
+sed -i '' 's/+[[:digit:]]\{2\}:[[:digit:]]\{2\}//g' $6
+
+Q1="CREATE DATABASE IF NOT EXISTS $NEWDBASE;"
+Q2="GRANT ALL ON $NEWDBASE.* TO '$NEWUSER'@'$HOST' IDENTIFIED BY '$NEWPASS';"
 Q3="FLUSH PRIVILEGES;"
-Q4="USE $1;"
-Q5="SOURCE $4;"
-Q6="SOURCE $5;"
 
-SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
+Q4="USE $NEWDBASE;"
+Q5="SOURCE $5;"
+Q6="SOURCE $6;"
+
+SQL1="${Q1}${Q2}${Q3}"
+SQL2="${Q4}${Q5}${Q6}"
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: $0 dbname dbuser dbpass schemafile datafile"
+  echo "Usage: $0 dbname dbuser dbpass dbPort schemafile datafile"
   exit $E_BADARGS
 fi
 
-$MYSQL -uroot -ppassword -e "$SQL"
+$MYSQL -uroot -p -P$PORT -h$HOST -e "$SQL1"
 
-ok "Database $1 and user $2 created with a password $3 with schema imported from $4 and data migrated from $5"
+$MYSQL -u$NEWUSER -p$NEWPASS -P$PORT -h$HOST -D$NEWDBASE -e "$SQL2"
+
+
+echo "Database $NEWDBASE and user $NEWUSER created with a password $NEWPASS with schema imported from $5 and data migrated from $6"
